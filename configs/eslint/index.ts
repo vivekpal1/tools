f27c9b9e99adab559eada7e96d38b58dc0ec8bc4
vivekpal1 <31vivekpal@gmail.com>
2023-11-14 22:33:40 +0530

    format

--- configs/eslint/index.ts
+++ configs/eslint/index.ts
@@ -1,1 +1,39 @@
-export * from './';
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["react", "@typescript-eslint"],
+  root: true,
+  rules: {
+    // e.g., "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        // Rules specific to TypeScript files
+      },
+    },
+  ],
+};
